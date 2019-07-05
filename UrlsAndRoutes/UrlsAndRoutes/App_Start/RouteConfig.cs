89e23e4f273566fd443105f6e3a96e602a1df78d
Yura Levchenko <yuralev240297@gmail.com>
2019-07-05 17:01:05 +0300

    Added new url routes

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,19 +11,15 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Home", action = "Index" });
+
+            routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home" });
+
+            routes.MapRoute("", "X{controller}/{action}");
+
             routes.MapRoute("MyRoute", "{controller}/{action}", new { controller = "Home", action = "Index" });
 
-
-
-
-
-            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute("", "Public/{controller}/{action}", new { controller = "Home", action = "Index" });
         }
     }
 }
