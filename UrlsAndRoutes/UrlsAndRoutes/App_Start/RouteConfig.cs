a75fb27c76c9ca3e8043121f70f1f78b046ab7be
Yura Levchenko <yuralev240297@gmail.com>
2019-07-10 17:06:54 +0300

    Prioritization of controllers

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,8 +11,13 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            Route myRoute = routes.MapRoute("AddControllerRoute", "Home/{action}/{id}/{*catchall}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "UrlsAndRoutes.AdditionalControllers" });
+            myRoute.DataTokens["UseNamespaceFallback"] = false;
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional });       
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "UrlsAndRoutes.Controllers" });       
         }
     }
 }
