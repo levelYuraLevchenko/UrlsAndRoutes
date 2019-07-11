d82206513285966eae57d75d6ef00f0e30664b09
Yura Levchenko <yuralev240297@gmail.com>
2019-07-11 16:38:59 +0300

    Using constraints based on types and values

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Mvc.Routing.Constraints;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -11,11 +13,21 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("ChromeRoute", "{*catchall}",
+                new { controller = "Home", action = "Index" },
+                new { customConstraint = new UserAgentConstraint("Chrome") },
+                new[] { "UrlsAndRoutes.AdditionalControllers" });
+
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 new {
                     controller = "^H.*", action = "^Index$|^About$",
-                    httpMethod = new HttpMethodConstraint("GET")
+                    httpMethod = new HttpMethodConstraint("GET"),
+                    id = new CompoundRouteConstraint(new IRouteConstraint[]
+                    {
+                        new AlphaRouteConstraint(),
+                        new MinLengthRouteConstraint(6)
+                    })
                 },
                 new[] { "UrlsAndRoutes.Controllers" });       
         }
---
+++ UrlsAndRoutes/UrlsAndRoutes/Infrastructure/UserAgentConstraint.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class UserAgentConstraint : IRouteConstraint
+    {
+        private string requairedUserAgent;
+
+        public UserAgentConstraint(string agentParam)
+        {
+            requairedUserAgent = agentParam;
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return httpContext.Request.UserAgent != null && httpContext.Request.UserAgent.Contains(requairedUserAgent);
+        }
+    }
+}
