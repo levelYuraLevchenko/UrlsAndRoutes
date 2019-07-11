0b6d64b5db729bda99313cecc9d0a5af43a4940b
Yura Levchenko <yuralev240297@gmail.com>
2019-07-11 17:31:14 +0300

    Applying route restrictions

--- UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -12,10 +12,16 @@ namespace UrlsAndRoutes.Controllers
             return View("ActionName");
         }
 
-        [Route("Users/Add/{user}/{id}")]
+        [Route("Users/Add/{user}/{id:int}")]
         public string Create(string user, int id)
         {
             return string.Format("User: {0}, ID: {1}", user, id);
+        }
+
+        [Route("Users/Add/{user}/{password:alpha:length(6)}")]
+        public string ChagePass(string user, string password)
+        {
+            return string.Format("ChangePass Method - User: {0}, Pass: {1}", user, password);
         }
 
         public ActionResult List()
