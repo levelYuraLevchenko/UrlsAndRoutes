c8fb6882363d748c7793bc6c040f88c93f77e5e8
Yura Levchenko <yuralev240297@gmail.com>
2019-07-15 17:43:26 +0300

    Project Preparation and Outbound URL Generation

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -15,9 +15,11 @@ namespace UrlsAndRoutes
         {
             routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional },              
-                new[] { "UrlsAndRoutes.Controllers" });       
+            routes.MapRoute("NewRoute", "App/Do{action}",
+                new { controller = "Home" });
+
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });       
         }
     }
 }
