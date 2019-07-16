c4d07e166abbf15bcbb34a827c5e4fb4d4328a85
Yura Levchenko <yuralev240297@gmail.com>
2019-07-16 16:23:48 +0300

    Passing additional values

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -15,9 +15,6 @@ namespace UrlsAndRoutes
         {
             routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute("NewRoute", "App/Do{action}",
-                new { controller = "Home" });
-
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional });       
         }
